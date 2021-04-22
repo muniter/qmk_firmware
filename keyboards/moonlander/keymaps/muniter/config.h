8419679dbba5fbd8cb702697264d0674cb71a512
Javier López <graulopezjavier@gmail.com>
2021-04-21 21:26:21 -0500

    Going home row modifiers

--- keyboards/moonlander/keymaps/muniter/config.h
+++ keyboards/moonlander/keymaps/muniter/config.h
@@ -2,24 +2,30 @@
   Set any config.h overrides for your specific keymap here.
   See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
 */
+//#define WAIT_FOR_USB
 #define ORYX_CONFIGURATOR
-#undef TAPPING_TERM
-#define TAPPING_TERM 180
 
 #define ONESHOT_TAP_TOGGLE 3
 
 #undef ONESHOT_TIMEOUT
 #define ONESHOT_TIMEOUT 1900
 
+// Mod tap configuration (For fast typist)
+// Typing term
+#define TAPPING_TERM 180
+// Avoids triggering the mod when not needed
 #define IGNORE_MOD_TAP_INTERRUPT
+// Triggers the mod when It should
 #define PERMISSIVE_HOLD
+// Do not repeat a ModTap key when held after a press
+#define TAPPING_FORCE_HOLD_PER_KEY
 
-#define USB_SUSPEND_WAKEUP_DELAY 0
-#define RGB_MATRIX_STARTUP_SPD 60
+// Disable RGB after 10 minutes without keypresses time in ms
+#define RGB_DISABLE_TIMEOUT 600000
 // Default mode for rgb effects
 #define RGB_MATRIX_STARTUP_MODE RGB_MATRIX_TYPING_HEATMAP
-// RGB off when PC is sleeping
-#define RGBLIGHT_SLEEP
 
 #define LEADER_TIMEOUT 300
 #define LEADER_PER_KEY_TIMING
+// Leader sends exact keycodes
+#define LEADER_KEY_STRICT_KEY_PROCESSING
--- keyboards/moonlander/keymaps/muniter/rules.mk
+++ keyboards/moonlander/keymaps/muniter/rules.mk
@@ -1,2 +1,8 @@
 # My rules
 LEADER_ENABLE = yes
+# Hat to add this so RGB doesn't get stuck on suspend
+NO_USB_STARTUP_CHECK = yes
+# Dynamic macro's enabled
+DYNAMIC_MACRO_ENABLE = yes
+# Locking keys
+KEY_LOCK_ENABLE = yes
